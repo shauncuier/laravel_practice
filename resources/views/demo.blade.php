36cd820df3a8a72503368545b07b246f38eaf773
Shaun <shauncuier@gmail.com>
2021-07-23 03:27:33 +0600

    Basic Routing Group

--- resources/views/demo.blade.php
+++
@@ -1,4 +0,0 @@
-
-<h1> First Name: {{ $firstkey }}</h1>
-<h1> Middle Name: {{ $middlekey }}</h1>
-<h1> Last Name: {{ $lastkey }}</h1>
--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,21 @@
 use App\Http\Controllers\DemoController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/name/{firstName}/{middleName}/{lastName}', [DemoController::class, 'MyName']);
+Route::group(['prefix'=>'account'], function(){
+
+    Route::get('/profile', function(){
+        return "Profile";
+    });
+    Route::get('/login',function(){
+        return "Login";
+    });
+    Route::get('/logout',function(){
+        return "Logout";
+    });
+    Route::get('/singup',function(){
+        return "Signup";
+    });
+    Route::get('/updateprofile',function(){
+        return "updateprofile";
+    });
+});
