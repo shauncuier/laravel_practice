a9d96fe5bded06c58429e399433819ed57a7f40d
Shaun <shauncuier@gmail.com>
2021-07-24 10:43:21 +0600

    Single Action Controller

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,8 @@
 <?php
 
-use App\Http\Controllers\DemoController;
+use App\Http\Controllers\MyController;
 use Illuminate\Support\Facades\Route;
 
-Route::group(['prefix'=>'account'], function(){
-
-    Route::get('/profile', function(){
-        return "Profile";
-    });
-    Route::get('/login',function(){
-        return "Login";
-    });
-    Route::get('/logout',function(){
-        return "Logout";
-    });
-    Route::get('/singup',function(){
-        return "Signup";
-    });
-    Route::get('/updateprofile',function(){
-        return "updateprofile";
-    });
-});
-
+Route::get('/', MyController::class);
 
 
