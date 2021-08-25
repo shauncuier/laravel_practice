86f3a4a142d7a1d9c5a996ac3e98dcb59eb3907d
Shaun <shauncuier@gmail.com>
2021-08-26 02:38:05 +0600

    Partial commit

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
 use App\Http\Controllers\MyController;
+use App\Http\Controllers\PhotoController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', MyController::class);
+//Route::get('/', MyController::class);
 
+
+Route::resource('photos', PhotoController::class);
 
