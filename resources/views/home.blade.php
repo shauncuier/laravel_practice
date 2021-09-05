20f29d51883084f9513a34fce8fab39fb72aae25
Shaun <shauncuier@gmail.com>
2021-09-06 03:00:40 +0600

    Loop Propartis

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,5 @@
-@if ($LoginStatusKey === true)
-    <h1>You Are Loged in</h1>
-@elseif($LoginStatusKey===false)
-    <h1>You Are not Loged in</h1>
-@else
-    <h1>Status not Found</h1>
+@foreach ($DataKey as $CountryName)
+@if($loop->last)
+{{ $CountryName }}
 @endif
+@endforeach
