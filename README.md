cd8d66e753ce9c06ad251fcdc765aafe591527d2
Shaun <shauncuier@gmail.com>
2021-09-06 02:04:51 +0600

    Pass Data

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 ### New controller Style in Laravel 8 -> 
     Route::get('myself', [HomeController::class,'MySelf']);
+
+# Data Pass
+    {{ With out HTML }}
+    {!! with HTML !!}
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 
-Route::get('/', [HomeController::class,'ShowHome']) ;
+Route::get('/',[HomeController::class,'ShowHome']);
 
 
 
