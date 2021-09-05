a7aeb83ae5c165e36408b127e98beb318fc2afab
Shaun <shauncuier@gmail.com>
2021-09-06 02:45:21 +0600

    If else

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,8 +6,11 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    function ShowHome(){
-        $Data=array('Bangladesh','India', 'Canada','Usa','UAE');
-        return view('home', ['DataKey'=>$Data]);
+    function ShowHome()
+    {
+        $Data = array('Bangladesh', 'India', 'Canada', 'Usa', 'UAE');
+        $LoginStatus = 'test';
+
+        return view('home', ['DataKey' => $Data, 'LoginStatusKey' => $LoginStatus]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,7 @@
-@forelse($DataKey as $CountryName)
-    <ul>
-        <li>{{ $CountryName }}</li>
-    </ul>
-
-@empty
-    <p>No Data Found</p>
-@endforelse
+@if ($LoginStatusKey === true)
+    <h1>You Are Loged in</h1>
+@elseif($LoginStatusKey===false)
+    <h1>You Are not Loged in</h1>
+@else
+    <h1>Status not Found</h1>
+@endif
