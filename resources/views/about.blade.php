22dfc89b59c7bbcf6dd1864dae1371f11cd953ec
Shaun <shauncuier@gmail.com>
2021-10-13 03:12:29 +0600

    45 Done

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,7 +2,30 @@
 @section('title', 'About')
 @section('active', 'active')
 @section('content')
-<div class="main-body container-fluid">
+    <div class="main-body container-fluid">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis quisquam explicabo voluptas optio,
+                        enim nemo corrupti, quibusdam repellat est esse velit? Incidunt expedita nihil vero aliquam
+                        temporibus iusto corrupti delectus?</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem maiores veniam rerum quasi
+                        obcaecati reprehenderit ipsa aliquam expedita, praesentium mollitia!</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, magni dolores pariatur quos impedit
+                        beatae ipsum aliquid eveniet repudiandae, at unde nemo dolorem reprehenderit veniam.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, magni dolores pariatur quos impedit
+                        beatae ipsum aliquid eveniet repudiandae, at unde nemo dolorem reprehenderit veniam. Lorem ipsum
+                        dolor, sit amet consectetur adipisicing elit. Hic iste placeat dicta laudantium a nemo nulla nam at
+                        voluptatum molestias!</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, magni dolores pariatur quos impedit
+                        beatae ipsum aliquid eveniet repudiandae, at unde nemo dolorem reprehenderit veniam. Lorem ipsum
+                        dolor sit amet, consectetur adipisicing elit. Expedita, voluptatibus?</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, magni dolores pariatur quos impedit
+                        beatae ipsum aliquid eveniet repudiandae, at unde nemo dolorem reprehenderit veniam. Lorem ipsum
+                        dolor sit amet, consectetur adipisicing elit. Expedita, voluptatibus?</p>
 
-</div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
