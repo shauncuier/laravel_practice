87c5068a02a4aef9683aef86e49c73917333bd00
Shaun <shauncuier@gmail.com>
2021-10-27 01:14:07 +0600

    #

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -1,7 +1,65 @@
 @extends('layout.app')
 @section('title', 'Portfolio')
 @section('content')
-<div class="main-body container-fluid">
-<h1>Portfolio</h1> 
-</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 p-2">
+                <div class="card" style="width: 100%;">
+                    <img src="{{ asset('images/lumen.png')}}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                            the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 p-2">
+                <div class="card" style="width: 100%;">
+                    <img src="{{ asset('images/lumen.png')}}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                            the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 p-2">
+                <div class="card" style="width: 100%;">
+                    <img src="{{ asset('images/lumen.png')}}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                            the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 p-2">
+                <div class="card" style="width: 100%;">
+                    <img src="{{ asset('images/lumen.png')}}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                            the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-3 p-2">
+                <div class="card" style="width: 100%;">
+                    <img src="{{ asset('images/lumen.png')}}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                            the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 @endsection
